bcb3883ff9cd24b69e9af210ff9cc0cbc5cd605d
ramchat007 <ramchat007@gmail.com>
2020-04-07 11:17:31 +0530

    session 2

--- src/app/document-mom/document-mom.component.ts
+++ src/app/document-mom/document-mom.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgbNavChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
 
-import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import {NgbCalendar, NgbDate, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import { MeetingdataService } from '../../assets/shared/services/meetingdata.service';
 import { NavbarService } from '../../assets/shared/services/navbar.service';
@@ -21,15 +21,11 @@ export class DocumentMomComponent implements OnInit {
     private metingdataService: MeetingdataService,
     private calendar: NgbCalendar,
     config: NgbAccordionConfig,
-    private fb: FormBuilder,
     public nav: NavbarService) {
       config.closeOthers = true;
       config.type = 'info';
    }
 
-  get contactFormGroup() {
-    return this.form.get('contacts') as FormArray;
-  }
   active = 1;
   meridian = true;
   model: NgbDateStruct;
@@ -39,12 +35,7 @@ export class DocumentMomComponent implements OnInit {
   internalParticipants = [];
   showlisting = true;
   session = 1;
-
-
-
-
-  public form: FormGroup;
-  public contactList: FormArray;
+  Session2 = false;
 
   public meetingDataEdit = [
     {
@@ -105,76 +96,6 @@ export class DocumentMomComponent implements OnInit {
   isWeekend = (date: NgbDate) =>  this.calendar.getWeekday(date) >= 6;
   // type: ['email', Validators.compose([Validators.required])], // i.e Email, Phone
 
-  // contact formgroup
-  createContact(): FormGroup {
-    return this.fb.group({
-        sessionTopic: new FormControl('', Validators.compose([Validators.required])),
-        presenter: new FormControl(''),
-        externalPresenter: new FormControl(''),
-        sessionStartTime: new FormControl(''),
-        sessionEndTime: new FormControl(''),
-        meetingStartDate: new FormControl(''),
-        internalParticipants: new FormGroup(
-          {
-          name: new FormControl(''), designation: new FormControl('')
-          }),
-        externalParticipants: new FormGroup(
-          {
-          name: new FormControl(''), extDesignation: new FormControl('')
-          }),
-        attachments: new FormControl(''),
-        actionItems: new FormGroup({
-          description: new FormControl(''),
-          type: new FormControl(''),
-          actionOwner: new FormControl(''),
-          targetDate: new FormControl(''),
-          priority: new FormControl(''),
-          remarks: new FormControl('')
-        })
-    });
-  }
-
-  // add a contact form group
-  addContact() {
-    this.contactList.push(this.createContact());
-  }
-
-  // add new session topic
-  addNew() {
-
-  }
-  // remove contact from group
-  removeContact(index) {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    this.contactList.removeAt(index);
-  }
-
-  // triggered to change validation of value field type
-  changedFieldType(index) {
-    let validators = null;
-
-    if (this.getContactsFormGroup(index).controls.type.value === 'email') {
-      validators = Validators.compose([Validators.required, Validators.email]);
-    } else {
-      validators = Validators.compose([
-        Validators.required,
-        Validators.pattern(new RegExp('^\\+[0-9]?()[0-9](\\d[0-9]{9})$')) // pattern for validating international phone number
-      ]);
-    }
-
-    // this.getContactsFormGroup(index).controls.value.setValidators(
-    //   validators
-    // );
-
-    // this.getContactsFormGroup(index).controls.value.updateValueAndValidity();
-  }
-
-  // get the formgroup under contacts form array
-  getContactsFormGroup(index): FormGroup {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    const formGroup = this.contactList.controls[index] as FormGroup;
-    return formGroup;
-  }
 
   // method triggered when form is submitted
   submit() {
@@ -193,16 +114,6 @@ export class DocumentMomComponent implements OnInit {
     });
     // console.log(this.internalParticipants);
 
-
-    this.form = this.fb.group({
-      name: [null, Validators.compose([Validators.required])],
-      organization: [null],
-      contacts: this.fb.array([this.createContact()])
-    });
-
-    // set contactlist to this field
-    this.contactList = this.form.get('contacts') as FormArray;
-
     this.nav.show();
   }
 
@@ -210,6 +121,10 @@ export class DocumentMomComponent implements OnInit {
 
   addMoreDesc() {
 
+  }
+
+  addSession() {
+    this.Session2 = true;
   }
 
   onSubmit() {
